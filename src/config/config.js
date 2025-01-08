3371f50c6a4e608470436e596f6d37c82f265b14
Cathy Liu <cathy30197@gmail.com>
2025-01-08 14:28:05 +0800

    Update backend url to https

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   env: {
-    API_URL: 'http://ec2-13-211-141-252.ap-southeast-2.compute.amazonaws.com:8080'
+    API_URL: 'https://ec2-13-211-141-252.ap-southeast-2.compute.amazonaws.com'
   },
 };
